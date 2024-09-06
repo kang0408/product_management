655497e8dad9604c24805ab98f46b650da717121
Kang0408 <trandanhkhang482004@gmail.com>
2024-09-06 09:03:35 +0700

    Thêm ghi chú cho controller

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../../controller/client/poduct.controller");
+const controller = require("../../controller/client/product.controller");
 
 router.get("/", controller.index);
 

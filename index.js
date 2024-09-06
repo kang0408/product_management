cafe1b78e88d7d15fed02826d822799f57d332ff
Kang0408 <trandanhkhang482004@gmail.com>
2024-09-06 08:41:10 +0700

    Tạo trang tổng quan (2)

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
@@ -13,6 +15,8 @@ const port = process.env.PORT; // cổng
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.use(express.static("public"));
 

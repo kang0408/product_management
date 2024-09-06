0e9162c57e3fc642382b3bf7f478b7741ad89324
Kang0408 <trandanhkhang482004@gmail.com>
2024-09-06 21:45:23 +0700

    Tính năng tìm kiếm (FE)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -16,3 +16,19 @@ if (buttonStatus.length > 0) {
   });
 }
 // End button status
+
+// Form Search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  const url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+
+    if (keyword) url.searchParams.set("keyword", keyword);
+    else url.searchParams.delete("keyword");
+
+    window.location.href = url;
+  });
+}
+// End Form Search

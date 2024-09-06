ca56a10a5e6aec95ab095fb740b2a50cb93590b9
Kang0408 <trandanhkhang482004@gmail.com>
2024-09-06 09:27:48 +0700

    Lấy data giao diện in ra màn hình

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,6 +1,12 @@
+const Product = require("../../models/product.model");
+
 // [GET] admin/products
-module.exports.products = (req, res) => {
+module.exports.products = async (req, res) => {
+  const products = await Product.find({});
+
+  console.log(products);
   res.render("admin/pages/products/index", {
     pageTitle: "Trang sản phẩm",
+    products: products,
   });
 };

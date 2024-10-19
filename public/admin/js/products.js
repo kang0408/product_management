79972ab0e7976c7025f8e0d5acaccab687b415a0
Kang0408 <trandanhkhang482004@gmail.com>
2024-10-19 08:02:25 +0700

    fix error

--- public/admin/js/products.js
+++ public/admin/js/products.js
@@ -1,10 +1,10 @@
 const changeStatusButton = document.querySelectorAll("[button-change-status]");
 const formChangeStatus = document.querySelector("#form-change-status");
-const path = formChangeStatus.getAttribute("path");
 
 if (changeStatusButton) {
   changeStatusButton.forEach((button) => {
     button.addEventListener("click", () => {
+      const path = formChangeStatus.getAttribute("path");
       const statusCurrent = button.getAttribute("data-status");
       const id = button.getAttribute("data-id");
 
--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,11 @@ const tags = document.querySelectorAll(".form-check-input");
 const buttonSearch = document.querySelector(".btn-search");
 const buttonRefresh = document.querySelector(".btn-refresh");
 
-buttonSearch.addEventListener("click", () => {
-  tags.forEach((tag) => {
-    if (tag.checked) console.log(tag.value);
+if (buttonSearch) {
+  buttonSearch.addEventListener("click", () => {
+    tags.forEach((tag) => {
+      if (tag.checked) console.log(tag.value);
+    });
   });
-});
+}
 // End posts
